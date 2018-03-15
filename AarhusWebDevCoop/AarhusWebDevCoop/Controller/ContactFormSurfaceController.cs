f90499349f044cc169da0b3be723cc2ee8d0a3a6
CHANDNI SHAW <chandnishawimps89@gmail.com>
2018-03-15 17:39:19 +0100

    initial commit

---
+++ AarhusWebDevCoop/AarhusWebDevCoop/Controller/ContactFormSurfaceController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controller
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+		// GET: Default 
+		public ActionResult Index()
+		{
+			return PartialView("ContactForm", new ContactForm());
+		}
+
+		[HttpPost]
+		public ActionResult HandleFormSubmit(ContactForm model)
+		{
+			//send email
+			MailMessage message = new MailMessage();
+			message.To.Add("chandnishawimps89@gmail.com");
+			message.Subject = model.Subject;
+			message.From = new System.Net.Mail.MailAddress(model.Email, model.Name);
+			message.Body = model.Message;
+
+			using (SmtpClient smtp = new SmtpClient())
+			{
+				smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+				smtp.UseDefaultCredentials = false;
+				smtp.EnableSsl = true;
+				smtp.Host = "smtp.gmail.com";
+				smtp.Port = 587;
+				smtp.Credentials = new System.Net.NetworkCredential("chandnishawimps89@gmail.com", "iyvieuljeovrhybc");
+				smtp.EnableSsl = true;
+
+				// send mail
+				smtp.Send(message);
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return CurrentUmbracoPage();
+			}
+
+			//create comment
+			var comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+			comment.SetValue("name1", model.Name);
+			comment.SetValue("email", model.Email);
+			comment.SetValue("subject", model.Subject);
+			comment.SetValue("message1", model.Message);
+
+			//save
+			Services.ContentService.SaveAndPublishWithStatus(comment);
+
+
+			TempData["success"] = true;
+			return RedirectToCurrentUmbracoPage();
+		}
+	}
+}
---
+++ AarhusWebDevCoop/AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+	public class ContactForm
+	{
+		[Required(ErrorMessage = "Please enter your name")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Please enter email address")]
+		[Display(Name = "Email")]
+		[RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", 
+			ErrorMessage = "Please enter correct email address")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Please enter a subject")]
+		public string Subject { get; set; }
+
+		[Required(ErrorMessage = "Please enter a message")]
+		public string Message { get; set; }
+	}
+}
